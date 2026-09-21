d92c7fbf71fc81880c58ba2183d05b2dbd5d2418
Andrew <andrewgtc2005@gmail.com>
2026-09-21 13:10:14 +0530

    Phase 4:  - added gracefull shutdown to fastapi

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI,HTTPException,Depends
+from contextlib import asynccontextmanager
 from fastapi.responses import RedirectResponse
 from .init_db import init_db
 from .schema import ShortenURLRequest
@@ -8,16 +9,22 @@ from .db import get_db
 from .cache import get_cached_url, cache_url
 from dotenv import load_dotenv
 load_dotenv()
+import asyncio
 
 import os
 
-app = FastAPI()
 
-
-
-@app.on_event("startup")
-async def startup():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("APPLICATION STARTED")
     await init_db()
+
+    yield
+
+    print("APPLICATION SHUTTING DOWN")
+
+
+app = FastAPI(lifespan=lifespan)
 
 @app.get("/")
 async def read_root():
@@ -28,6 +35,16 @@ async def shorten_url(payload: ShortenURLRequest,db:AsyncSession = Depends(get_d
     short_code = await insert_url(db,str(payload.url))
     await cache_url(short_code,str(payload.url))
     return f"http://{os.environ.get("HOST")}:{os.environ.get("PORT")}/{short_code}"
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
+
+@app.get("/slow")
+async def slow():
+    await asyncio.sleep(10)
+    return {"status": "done"}
 
 @app.get("/{short_code}")
 async def redirect_url(short_code:str,db:AsyncSession = Depends(get_db)):
@@ -44,6 +61,3 @@ async def redirect_url(short_code:str,db:AsyncSession = Depends(get_db)):
     return RedirectResponse(url=original_url)
 
 
-@app.get("/health")
-def health():
-    return {"status": "ok"}
